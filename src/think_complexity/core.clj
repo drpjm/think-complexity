96676eedb18743e6e68b3e4e5df25d94112d1475
Patrick Martin <prof.pmartin10@gmail.com>
2014-07-29 21:29:43 -0400

    Removed outer files.

--- src/think_complexity/core.clj
+++
@@ -1,2 +0,0 @@
-(ns think-complexity.core)
-
--- test/think_complexity/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns think-complexity.core-test
-  (:require [clojure.test :refer :all]
-            [think-complexity.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
