b366bfda49f4e87246a9b6377671861628063bf8
Patrick Martin <prof.pmartin10@gmail.com>
2014-07-31 22:47:45 -0400

    Added functionality to the graph fucntions: get and remove edges from a graph.

--- src/think_complexity/chapter02/core.clj
+++ src/think_complexity/chapter02/core.clj
@@ -1,9 +1,9 @@
 (ns think-complexity.chapter02.core)
 
 ; A graph can be represented as a map of maps data structure:
-; vs - set of vertices
-; es - set of vertex pairs
-(defn create-graph [vs es]
+; vs - set of vertices, which are labels
+; es - set of vertex pairs, which are pairs of vertices
+(defn create-simple-graph [vs es]
   (let [new-graph {}]
     (assoc new-graph :V (set vs) :E (set es))))
 
@@ -12,3 +12,24 @@
 
 (defn add-edge [graph e]
   (assoc graph :E (conj (:E graph) e)))
+
+(defn edge-as-string [e]
+  (str (first e) " -> " (last e)))
+
+(defn has-edge? [graph v1 v2]
+  (if (contains? (:E graph) #{v1 v2})
+    true
+    false))
+
+(defn get-edge [graph v1 v2]
+  "This function returns the edge that connects v1 and v2, or nil
+  if it does not exist."
+  (if (has-edge? graph v1 v2)
+    #{v1 v2}
+    nil))
+
+(defn remove-edge [graph v1 v2]
+  (if (has-edge? graph v1 v2)
+    (assoc graph :E (disj (:E graph) #{v1 v2}))
+    graph))
+
--- test/think_complexity/chapter02/core_test.clj
+++ test/think_complexity/chapter02/core_test.clj
@@ -1,6 +1,7 @@
 (ns think-complexity.chapter02.core-test
   (:require [think-complexity.chapter02.core :refer :all]))
 
-(def new-graph
-  (create-graph [:v1 :v2] [[:v1 :v2] [:v2 :v2]]))
+(def test-graph
+  (create-simple-graph  [:v1 :v2] [#{:v1 :v2} #{:v2 :v3}]))
 
+(map edge-as-string (:E test-graph))
