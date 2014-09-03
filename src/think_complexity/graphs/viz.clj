77bcf5889df227f0f036c735384d328c5bdfa4dd
Patrick Martin <prof.pmartin10@gmail.com>
2014-09-03 07:10:34 -0400

    Finished making a function that generates a rhizome compatible graph that represents a regular graph. However, there may be a bug with the add-regular-edges function.

--- src/think_complexity/graphs/viz.clj
+++ src/think_complexity/graphs/viz.clj
@@ -3,15 +3,27 @@
             [rhizome.viz :as r]
             [think-complexity.graphs.core :as my-g]))
 
-(defn to-rhizome-graph [my-graph]
-  "Function returns a structure easily used by rhizome for graph visualization."
-  (let [vs (my-g/vertices my-graph)
-        adj-vs (map (fn [v] (my-g/out-vertices my-graph v)) vs)]
-    (zipmap vs adj-vs)))
+;(def g (my-g/create-simple-graph [:v1 :v2 :v3 :v4 ] []))
 
-(def g (my-g/create-simple-graph [:v1 :v2 :v3] [#{:v1 :v2} #{:v2 :v3}]))
+;(def reg-g (my-g/add-regular-edges g 1))
 
-(def rhizome-graph (to-rhizome-graph g))
+(defn- to-rhizome-graph [my-graph]
+  (loop [rhizome-graph {}
+         my-graph-vs (my-g/vertices my-graph)]
+    (if (empty? my-graph-vs)
+      rhizome-graph
+      (let [curr-node (first my-graph-vs)
+            out-vs (my-g/out-vertices my-graph curr-node)
+            new-dests (filter (fn [v] (not (contains? rhizome-graph v))) out-vs)]
+        (recur
+         (assoc rhizome-graph curr-node new-dests)
+         (disj my-graph-vs curr-node))))))
 
-(r/view-graph (keys rhizome-graph) rhizome-graph
-    :node->descriptor (fn [n] {:label n}))
+;(to-rhizome-graph reg-g)
+
+(defn view-graph [my-graph]
+  (r/view-graph (keys (to-rhizome-graph my-graph)) (to-rhizome-graph my-graph)
+    :node->descriptor (fn [n] {:label n})
+    :edge->descriptor (fn [s d] {:arrowhead "none"})))
+
+;(view-graph reg-g)
